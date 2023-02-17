e266d821b2de715fea1d88cf1155ad945c783753
Emirhan Dilber <113054383+emirhandilber@users.noreply.github.com>
2023-02-17 16:42:51 +0300

    Add files via upload

---
+++ Caesar cipher.py
@@ -0,0 +1,14 @@
+plainText=input('İnput the text : ')
+shift=int(input('Choose the shifter : '))
+def caesar(plainText, shift): 
+  cipherText = ""
+  for ch in plainText:
+    if ch.isalpha():
+      stayInAlphabet = ord(ch) + shift 
+      if stayInAlphabet > ord('z'):
+        stayInAlphabet -= 26
+      finalLetter = chr(stayInAlphabet)
+      cipherText += finalLetter
+  print ("Your ciphertext is: ", cipherText)
+  return cipherText
+caesar(plainText, shift)
